09f30168afbc25a692d277d6f84c717d8612a01c
mat0606 <mat0606@hotmail.com>
2023-01-01 07:08:53 -0800

    add in csi,volumes, files

--- sidebars.js
+++ sidebars.js
@@ -4,12 +4,23 @@ module.exports = {
     {
       type: 'category',
       label: 'OpenShift Installation',
-      items: ["ocp_upi_install/ocp_upi_install","ocp_ipi_install/ocp_ipi_install"]
+      items: ["ocp_upi_install/ocp_upi_install","ocp_ipi_install/ocp_ipi_install","ocp_assisted_install/ocp_assisted_install"]
     },
     {
       type: 'category',
-      label: 'Nutanix Storage CSI',
-      items: ['ocp_ntnx_hci/ocp_csi','optional_labs/ocp_image_registry'],
+      label: 'Nutanix CSI Deployment',
+      items: ['ocp_ntnx_hci/ocp_csi', 'ocp_ntnx_hci/ocp_csi2','ocp_ntnx_hci/ocp_csi_prometheus']
+    },
+    "ocp_project/ocp_project",
+    {
+      type: 'category',
+      label: 'Nutanix Volume Deployment',
+      items: ['ocp_ntnx_hci/ocp_sc_volume', 'ocp_ntnx_hci/ocp_sc_volume2','ocp_ntnx_hci/ocp_volume_pvc'],
+    },
+    {
+      type: 'category',
+      label: 'Nutanix Files Deployment',
+      items: ['ocp_ntnx_hci/ocp_sc_files', 'ocp_ntnx_hci/ocp_sc_files2','ocp_ntnx_hci/ocp_files_pvc','optional_labs/ocp_image_registry'],
     },
     "ocp_ndb/ocp_ndb",
     {
@@ -19,15 +30,10 @@ module.exports = {
     },
     {
       type: 'category',
-      label: 'OCP with Nutanix Cloud Manager (NCM)',
-      items: ['ocp_ntnx_hci/import_bp','ocp_ntnx_hci/prov_vm','ocp_ntnx_hci/ocp_vms','ocp_ntnx_hci/ocp_scale_out_in'],
-    },
-    "ocp_assisted_install/ocp_assisted_install",
-    {
-      type: 'category',
       label: 'OCP Optional Labs',
       items: ['optional_labs/ocp_vms_2','optional_labs/ocp_idp'],
     },
+    'optional_labs/ocp_image_registry',
     {
       type: 'category',
       label: 'Tools VMs',
